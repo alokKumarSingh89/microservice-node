2f75d8bda327066f8f506618cb3b6f83c8bd1513
alok.singh <apple@applesmacbook.local>
2022-03-15 12:34:12 -0700

    main app completed for rest

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -11,11 +11,11 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
   app.enableCors({
     origin: 'http://localhost:4200',
   });
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 4001;
+  const port = process.env.PORT || 4002;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
