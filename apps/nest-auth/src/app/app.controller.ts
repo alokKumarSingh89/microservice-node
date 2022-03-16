7684565100aaedd29a12212e290767fe6352bf09
alok.singh <apple@applesmacbook.local>
2022-03-16 06:22:18 -0700

    local login done

--- apps/nest-auth/src/app/app.controller.ts
+++ apps/nest-auth/src/app/app.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(): any {
-    return {};
+  login(@Request() req): any {
+    return req.user;
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Get('protected')
   protectedUrl(): any {
     return {};
--- apps/nest-auth/src/app/user/user.service.ts
+++ apps/nest-auth/src/app/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'alok kumar singh',
+      username: 'alok',
+      password: 'alok',
+    },
+    {
+      id: 2,
+      name: 'Monika Kumar Singh',
+      username: 'monika',
+      password: 'password',
+    },
+  ];
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
